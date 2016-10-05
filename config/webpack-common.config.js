1e21eb290a6ab5c49b93f1d050be12f0edf8ced9
Pavan Podila <pavan.podila@gmail.com>
2016-10-04 21:18:07 -0400

    upgrading packages

--- config/webpack-common.config.js
+++ config/webpack-common.config.js
@@ -13,7 +13,9 @@ module.exports = {
 
             {
                 test: /\.scss/,
-                loader: ExtractTextPlugin.extract('css!sass')
+                loader: ExtractTextPlugin.extract({
+                    loader: ['css', 'sass']
+                })
             }
         ],
     },
