e43039a7984562a3b7130bb14b7e94a9765f1c0b
Patrick Allen <AllenPatrick86@gmail.com>
2018-08-30 12:28:51 -0700

    Convert ActionElement to tsx

---
+++ src/controls/ActionElement.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export interface ActionElementProps {
+  label?: string;
+  className?: string;
+  handleOnClick?: (val: React.MouseEvent<HTMLButtonElement>) => void;
+  title?: string;
+}
+
+const ActionElement = (props: ActionElementProps): JSX.Element => {
+  this.displayName = 'ActionElement';
+  const { label, className, handleOnClick, title } = props;
+
+  return (
+    <button
+      className={className}
+      title={title}
+      onClick={(e) => handleOnClick(e)}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default ActionElement;
