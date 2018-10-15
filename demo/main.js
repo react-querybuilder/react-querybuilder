e98f659fc92c03708b7061801b3fe6a82918813c
Dallas Larsen <dallas@thegalacticdesignbureau.com>
2018-10-15 13:58:16 -0700

    Update main.js to show working QB updates fields and query  from QB willRecievePorps

--- demo/main.js
+++ demo/main.js
@@ -3,7 +3,7 @@ import QueryBuilder from '../src/index';
 import ReactDOM from "react-dom";
 import React from 'react';
 
-const fields = [
+const fields1 = [
     {name: 'firstName', label: 'First Name'},
     {name: 'lastName', label: 'Last Name'},
     {name: 'age', label: 'Age'},
@@ -13,30 +13,101 @@ const fields = [
     {name: 'twitter', label: 'Twitter'},
     {name: 'isDev', label: 'Is a Developer?', value: false},
 ];
+const fields2 = [
+    {name: 'contactFirstName', label: 'Contact First Name'},
+    {name: 'contactLastName', label: 'Contact Last Name'},
+    {name: 'contactEmail', label: 'Contact Email'},
+];
+const fieldQuerySets = {
+     set1: { 
+         fields: fields1, 
+         query: {
+                    "id": "g-fe7a7130-9d9f-4ec8-b5d3-79b5f0aff350",
+                    "rules": [
+                    {
+                        "id": "r-2c153586-5044-4ceb-9f36-cee6b06b035f",
+                        "field": "firstName",
+                        "value": "Steve",
+                        "operator": "="
+                    }
+                    ],
+                    "combinator": "and"
+                }
+            }, 
 
+     set2:{ 
+         fields: fields2, 
+         query: {
+                    "id": "g-fe7a7130-9d9f-4ec8-b5d3-79b5f0aff350",
+                    "rules": [
+                    {
+                        "id": "r-2c153586-5044-4ceb-9f36-cee6b06b035f",
+                        "field": "contactFirstName",
+                        "value": "Sally",
+                        "operator": "="
+                    }
+                    ],
+                    "combinator": "and"
+                }
+    
+    },
+
+}
 class RootView extends React.Component {
     constructor() {
         super();
         this.state = {
-            query: {}
+            query: {
+                set1: fieldQuerySets.set1.query,
+                set2: fieldQuerySets.set2.query
+            },
+            fields: {
+                set1:fieldQuerySets.set1.fields,
+                set2:fieldQuerySets.set2.fields
+            },
+            currentSet: "set1"
         };
     }
+    componentDidMount = () =>{
+       //console.log(fieldQuerySets.set2.query);
+    }
+   
+    handleChange = (e) =>{
+        e.persist()
+        this.setState((prevState)=>{ 
+            return {
+                    currentSet: e.target.value, 
+                } 
+        })
 
+    }
     render() {
         let controlElements = {
             valueEditor: this.customValueEditor()
         }
+        const { currentSet } = this.state;
+
+       // console.log(currentSet,this.state.query[currentSet] );
+
         return (
+        
             <div className="flex-box">
+                <div><select name="fieldToggle" value={this.state.currentSet} onChange={this.handleChange}>
+                        <option value="set1">Set 1</option>
+                        <option value="set2">Set 2</option>
+                      </select>
+                </div>
                 <div className="scroll">
-                    <QueryBuilder fields={this.props.fields}
+                    <QueryBuilder 
+                                  query={ this.state.query[currentSet]}
+                                  fields={this.state.fields[currentSet]}
                                   controlElements={controlElements}
                                   controlClassnames={{fields: 'form-control'}}
                                   onQueryChange={this.logQuery.bind(this)}/>
                 </div>
                 <div className="shrink query-log scroll">
                     <h4>Query</h4>
-                    <pre>{JSON.stringify(this.state.query, null, 2)}</pre>
+                    <pre>{JSON.stringify(this.state.query[this.state.currentSet], null, 2)}</pre>
                 </div>
             </div>
         );
@@ -68,10 +139,12 @@ class RootView extends React.Component {
     }
 
     logQuery(query) {
-        this.setState({query});
+        const currentQuery = {...this.state.query[this.state.currentSet]}
+        currentQuery.query = query;
+        this.setState({ currentQuery  });
     }
 
 }
 
-ReactDOM.render(<RootView fields={fields}/>, document.querySelector('.container'));
+ReactDOM.render(<RootView />, document.querySelector('.container'));
 
