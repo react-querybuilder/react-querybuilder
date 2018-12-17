24c1a8191563c0c6586542ab1d73de29ec08f828
André Drougge <andre.drougge@gmail.com>
2018-12-17 17:48:09 +0000

    Small whitespace cleanups

--- src/QueryBuilder.test.js
+++ src/QueryBuilder.test.js
@@ -17,12 +17,13 @@ describe('<QueryBuilder />', () => {
             expect(QueryBuilder.prototype.componentWillMount.calledOnce).to.equal(true);
             dom.unmount();
         });
+
         it('calls componentDidMount', () => {
             sinon.spy(QueryBuilder.prototype, 'componentDidMount');
             const dom = mount(<QueryBuilder />);
             expect(QueryBuilder.prototype.componentDidMount.calledOnce).to.equal(true);
-
         });
+
         it('should render the root RuleGroup', () => {
             const dom = shallow(<QueryBuilder />);
             expect(dom.find('RuleGroup')).to.have.length(1);
@@ -33,18 +34,14 @@ describe('<QueryBuilder />', () => {
             const options = dom.find('select option');
             expect(options).to.have.length(2); // and, or
         });
+
         it('should call  onQueryChange',()=>{
             const dom = mount(<QueryBuilder onQueryChange={()=>{}} />);
             dom.instance()._notifyQueryChange(()=>{});
             dom.update();
             expect(dom.props().query).to.equal(null);
         });
-
-
-
     });
-
-
 
     describe('when initial query is provided', () => {
         let dom, query;
@@ -68,13 +65,10 @@ describe('<QueryBuilder />', () => {
             };
 
             dom = mount(<QueryBuilder query={query} fields={fields} />);
-
         });
 
         afterEach(() => {
-            if (dom) {
-                dom.unmount();
-            }
+            if (dom) dom.unmount();
         });
 
         it('should generate valid query', () => {
@@ -101,16 +95,13 @@ describe('<QueryBuilder />', () => {
             expect(validQuery.rules[0].id).to.equal('r-12345');
         })
 
-
         it('should contain a <Rule />', () => {
-
             const rule = dom.find('Rule');
             expect(rule).to.have.length(1);
         });
 
         it('should have the Rule with the correct props', () => {
             const rule = dom.find('Rule');
-
             expect(rule.props().field).to.equal('firstName');
             expect(rule.props().value).to.equal('Test');
             expect(rule.props().operator).to.equal('=');
@@ -118,33 +109,27 @@ describe('<QueryBuilder />', () => {
 
         it('should have a select control with the provided fields', () => {
             const rule = dom.find('Rule');
-
             expect(rule.find('.rule-fields option')).to.have.length(3);
         });
 
         it('should have a field selector with the correct field', () => {
             const rule = dom.find('Rule');
-
             expect(rule.find('.rule-fields select').props().value).to.equal('firstName');
         });
 
         it('should have an operator selector with the correct operator', () => {
             const rule = dom.find('Rule');
-
             expect(rule.find('.rule-operators select').props().value).to.equal('=');
         });
 
         it('should have an input control with the correct value', () => {
             const rule = dom.find('Rule');
-
             expect(rule.find('input').props().value).to.equal('Test');
         });
+    });
 
-
-});
     describe('when new props are passed',()=>{
         it('calls componentWillRecieveProps', () => {
-
             const newFields = [
                 { name: 'domainName', label: 'Domain Name' },
                 { name: 'ownerName', label: 'Owner Name' },
@@ -163,10 +148,10 @@ describe('<QueryBuilder />', () => {
                     }
                 ]
             };
+
             //First mount with props
             sinon.spy(QueryBuilder.prototype, 'componentWillReceiveProps');
             const dom = mount(<QueryBuilder />);
-
             expect(QueryBuilder.prototype.componentWillReceiveProps.calledOnce).to.equal(false);
 
             //componentWillRecieveProps
@@ -180,7 +165,6 @@ describe('<QueryBuilder />', () => {
             expect(dom.find('RuleGroup')).to.have.length(1);
             expect(rule.find('input').props().value).to.equal('www.example.com');
         });
-
     });
 
     describe('when initial operators are provided', () => {
@@ -217,24 +201,22 @@ describe('<QueryBuilder />', () => {
                                       fields={fields}
                                       query={query} />);
         });
+
         it('should get operators for field', () => {
             let operators = dom.state('schema').getOperators('firstName');
-            //console.log('OPS',operators);
             expect(operators.length).to.equal(4);
         });
+
         it('should use the given operators', () => {
             const operatorOptions = dom.find('Rule').find('.rule-operators option');
-
             expect(operatorOptions.length).to.equal(4);
         });
 
         it('should match the label of the first operator', () => {
             const operatorOption = dom.find('Rule').find('.rule-operators option').first();
-
             expect(operatorOption.text()).to.equal('Custom Is Null');
         });
     });
-
 
     describe('when calculating the level of a rule', function () {
         let dom;
