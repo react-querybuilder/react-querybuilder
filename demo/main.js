5bee5d1ce8a204531ead00172be9333ab34a3b8a
André Drougge <andre.drougge@gmail.com>
2019-02-11 14:22:09 +0200

    Refactor and simplify demo, showcasing how QB behaves with new props.

--- demo/main.js
+++ demo/main.js
@@ -1,135 +1,130 @@
 import '../src/query-builder.scss';
 import QueryBuilder from '../src/index';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import React from 'react';
 
-const fields1 = [
-    {name: 'firstName', label: 'First Name'},
-    {name: 'lastName', label: 'Last Name'},
-    {name: 'age', label: 'Age'},
-    {name: 'address', label: 'Address'},
-    {name: 'phone', label: 'Phone'},
-    {name: 'email', label: 'Email'},
-    {name: 'twitter', label: 'Twitter'},
-    {name: 'isDev', label: 'Is a Developer?', value: false},
-];
+const preparedFields = {
+  primary: [{ name: 'firstName', label: 'First Name' }, { name: 'lastName', label: 'Last Name' }],
+  secondary: [
+    { name: 'age', label: 'Age' },
+    { name: 'isMusician', label: 'Is a musician' },
+    { name: 'instrument', label: 'Instrument' }
+  ],
+  generic: [
+    { name: 'firstName', label: 'First name' },
+    { name: 'lastName', label: 'Last name' },
+    { name: 'age', label: 'Age' },
+    { name: 'gender', label: 'Gender' },
+    { name: 'height', label: 'Height' },
+    { name: 'job', label: 'Job' }
+  ]
+};
 
-const fields2 = [
-    {name: 'contactFirstName', label: 'Contact First Name'},
-    {name: 'contactLastName', label: 'Contact Last Name'},
-    {name: 'contactEmail', label: 'Contact Email'},
-];
-
-const fieldQuerySets = {
-    set1: {
-        fields: fields1,
-        query: {
-            "id": "g-fe7a7130-9d9f-4ec8-b5d3-79b5f0aff350",
-            "rules": [
-                {
-                    "id": "r-2c153586-5044-4ceb-9f36-cee6b06b035f",
-                    "field": "firstName",
-                    "value": "Steve",
-                    "operator": "="
-                }
-            ],
-            "combinator": "and"
-        }
-    },
-
-     set2: {
-        fields: fields2,
-        query: {
-            "rules": [
-                {
-                    "field": "contactFirstName",
-                    "value": "Sally",
-                    "operator": "="
-                }
-            ],
-            "combinator": "and"
-        }
-    }
+const preparedQueries = {
+  primary: {
+    id: 'g-8953ed65-f5ff-4b77-8d03-8d8788beb50b',
+    rules: [
+      {
+        id: 'r-32ef0844-07e3-4f3b-aeca-3873da3e208b',
+        field: 'firstName',
+        value: 'Steve',
+        operator: '='
+      },
+      {
+        id: 'r-3db9ba21-080d-4a5e-b4da-d949b4ad055b',
+        field: 'lastName',
+        value: 'Vai',
+        operator: '='
+      }
+    ],
+    combinator: 'and'
+  },
+  secondary: {
+    id: 'g-15e72d98-557f-4a09-af90-6d7afc05b0f7',
+    rules: [
+      {
+        field: 'age',
+        id: 'r-45b166dd-d69a-4008-9587-fe796aeda496',
+        operator: '>',
+        value: '28'
+      },
+      {
+        field: 'isMusician',
+        id: 'r-db6fded6-bd8c-4b4f-9a33-a00f7417a9a9',
+        operator: '=',
+        value: 'true'
+      },
+      {
+        field: 'instrument',
+        id: 'r-df23ba2b-e600-491d-967c-116ade6fe45e',
+        operator: '=',
+        value: 'Guitar'
+      }
+    ],
+    combinator: 'or'
+  },
+  generic: {
+    combinator: 'and',
+    rules: []
+  }
 };
 
 class RootView extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            query: {
-                set1: fieldQuerySets.set1.query,
-                set2: fieldQuerySets.set2.query
-            },
-            fields: {
-                set1:fieldQuerySets.set1.fields,
-                set2:fieldQuerySets.set2.fields
-            },
-            currentSet: "set1"
-        };
+  constructor(props) {
+    super(props);
+    this.handleQueryChange = this.handleQueryChange.bind(this);
+    this.state = {
+      query: preparedQueries.primary,
+      fields: preparedFields.primary
+    };
+  }
+
+  // Reloads a prepared query, a PoC for query updates by props change.
+  // If no target is supplied, clear query (generic query).
+  loadQuery(target) {
+    if (target) {
+      this.setState({
+        query: preparedQueries[target],
+        fields: preparedFields[target]
+      });
+    } else {
+      this.setState({
+        query: preparedQueries.generic,
+        fields: preparedFields.generic
+      });
     }
+  }
 
-    handleChange = (e) =>{
-        e.persist();
-        this.setState((prevState) => ({ currentSet: e.target.value }));
-    }
+  handleQueryChange(query) {
+    this.setState({ query });
+  }
 
-    render() {
-        let controlElements = {
-            valueEditor: this.customValueEditor()
-        }
-        const { currentSet } = this.state;
-
-        return (
-            <div className='flex-box-outer'>
-                <select className='set-changer' name="fieldToggle" value={this.state.currentSet} onChange={this.handleChange}>
-                    <option value="set1">Set 1</option>
-                    <option value="set2">Set 2</option>
-                </select>
-                <div className="flex-box">
-                    <div className="scroll">
-                        <QueryBuilder
-                            query={ this.state.query[currentSet]}
-                            fields={this.state.fields[currentSet]}
-                            controlElements={controlElements}
-                            controlClassnames={{fields: 'form-control'}}
-                            onQueryChange={this.logQuery.bind(this)}/>
-                    </div>
-                    <div className="shrink query-log scroll">
-                        <h4>Query</h4>
-                        <pre>{JSON.stringify(this.state.query[this.state.currentSet], null, 2)}</pre>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-    customValueEditor() {
-        let checkbox = class MyCheckbox extends React.Component {
-            render() {
-                if (this.props.field !== 'isDev' || this.props.operator !== '=') {
-                    return <input type="text"
-                                  value={this.props.value}
-                                  onChange={e => this.props.handleOnChange(e.target.value)} />
-                }
-
-                return (
-                    <span>
-                        <input type="checkbox"
-                               value={!!this.props.value}
-                               onChange={e => this.props.handleOnChange(e.target.checked)} />
-                    </span>
-                );
-            }
-        };
-        return checkbox;
-    }
-
-    logQuery(query) {
-        const currentQuery = {...this.state.query[this.state.currentSet]}
-        currentQuery.query = query;
-        this.setState({ currentQuery  });
-    }
-
+  render() {
+    return (
+      <div className="flex-box-outer">
+        <div className="control-panel">
+          <button onClick={() => this.loadQuery('primary')}>Load primary query</button>
+          <button onClick={() => this.loadQuery('secondary')}>Load secondary query</button>
+          <button onClick={() => this.loadQuery()}>Clear query</button>
+        </div>
+        <hr />
+        <div className="flex-box">
+          <div className="scroll">
+            <QueryBuilder
+              query={this.state.query}
+              fields={this.state.fields}
+              controlClassnames={{ fields: 'form-control' }}
+              onQueryChange={this.handleQueryChange}
+            />
+          </div>
+          <div className="shrink query-log scroll">
+            <h4>Query</h4>
+            <pre>{JSON.stringify(this.state.query, null, 2)}</pre>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<RootView />, document.querySelector('.container'));
