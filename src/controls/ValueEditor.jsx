46f57cdcdfc96f646df71d07b1ed8666bd4d39fb
Jake Boone <jakeboone02@gmail.com>
2019-08-27 10:10:10 -0700

    Fixed some bugs in ValueEditor
    Initial value for checkbox was not being respected.  Removed warning about keys for list items.  Fixed propTypes for value to `any`.

--- src/controls/ValueEditor.jsx
+++ src/controls/ValueEditor.jsx
@@ -20,14 +20,20 @@ const ValueEditor = ({
       return (
         <select onChange={(e) => handleOnChange(e.target.value)} value={value}>
           {values.map((v) => (
-            <option value={v.name}>{v.label}</option>
+            <option key={v.name} value={v.name}>
+              {v.label}
+            </option>
           ))}
         </select>
       );
 
     case 'checkbox':
       return (
-        <input type="checkbox" onChange={(e) => handleOnChange(e.target.checked)} value={!!value} />
+        <input
+          type="checkbox"
+          onChange={(e) => handleOnChange(e.target.checked)}
+          checked={!!value}
+        />
       );
 
     case 'radio':
@@ -61,7 +67,7 @@ ValueEditor.displayName = 'ValueEditor';
 ValueEditor.propTypes = {
   field: PropTypes.string,
   operator: PropTypes.string,
-  value: PropTypes.string,
+  value: PropTypes.any,
   handleOnChange: PropTypes.func,
   title: PropTypes.string,
   className: PropTypes.string,
