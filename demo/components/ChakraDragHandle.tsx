2585a2a210198eccb3d9b3a3dfc5376216136564
Jake Boone <jakeboone02@gmail.com>
2021-11-06 11:37:00 -0700

    Fix ChakraDragHandle

--- demo/components/ChakraDragHandle.tsx
+++ demo/components/ChakraDragHandle.tsx
@@ -1,3 +1,4 @@
+import { IconButton } from '@chakra-ui/button';
 import { DragHandleIcon } from '@chakra-ui/icons';
 import { forwardRef } from 'react';
 import type { DragHandleProps } from '../../src/types';
@@ -8,7 +9,7 @@ export default forwardRef<HTMLSpanElement, DragHandleProps>(function ChakraDragH
 ) {
   return (
     <span ref={dragRef} className={className} title={title}>
-      {enabled ? <DragHandleIcon /> : ''}
+      {enabled ? <IconButton aria-label={title} size="xs" icon={<DragHandleIcon />} /> : ''}
     </span>
   );
 });
