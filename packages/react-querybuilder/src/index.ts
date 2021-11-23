3f70b6e72a9663b0e778b60723c16444ce5aa53c
Jake Boone <jakeboone02@gmail.com>
2021-11-23 14:46:55 -0700

    Export all defaults and utils and add QueryBuilder as named export

--- packages/react-querybuilder/src/index.ts
+++ packages/react-querybuilder/src/index.ts
@@ -1,7 +1,8 @@
 import { QueryBuilder } from './QueryBuilder';
-export default QueryBuilder;
-export { defaultCombinators, defaultOperators, defaultTranslations } from './defaults';
-export { defaultValidator, defaultValueProcessor, findPath, formatQuery, parseSQL } from './utils';
+export { QueryBuilder };
+export * from './defaults';
+export * from './utils';
 export { Rule } from './Rule';
 export { ActionElement, DragHandle, NotToggle, ValueEditor, ValueSelector } from './controls/';
 export * from './types';
+export default QueryBuilder;
