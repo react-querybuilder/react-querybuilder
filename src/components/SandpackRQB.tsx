ab7cb6bf1838cd55bf84bbadb32d391c3a69aa78
Jake Boone <jakeboone02@gmail.com>
2021-12-31 00:56:52 -0700

    Use react-datepicker in "fallback" example instead of optgroups

--- src/components/SandpackRQB.tsx
+++ src/components/SandpackRQB.tsx
@@ -1,31 +1,89 @@
-import { Sandpack } from '@codesandbox/sandpack-react';
+import { Sandpack, SandpackFile, SandpackProps } from '@codesandbox/sandpack-react';
 import useThemeContext from '@theme/hooks/useThemeContext';
 import * as React from 'react';
 import './SandpackRQB.scss';
 
-export const SandpackRQB: React.FC = ({ children }) => {
+export const SandpackRQB: React.FC<SandpackProps> = ({ children, customSetup, options }) => {
   const { isDarkTheme } = useThemeContext();
-  const appCode = (React.Children.toArray(children) as React.ReactElement[])[0].props.children.props
-    .children as string;
-  const bkgdColor = isDarkTheme ? '#343a46' : 'white';
+  const codeSnippets = React.Children.toArray(children) as React.ReactElement[];
+  const bkgdColor = isDarkTheme ? '#343a46' : '#ffffff';
+  let hideStylesCSS = true;
+
+  const files = codeSnippets.reduce(
+    (result: Record<string, SandpackFile>, codeSnippet: React.ReactElement) => {
+      if (codeSnippet.props.mdxType !== 'pre') {
+        return result;
+      }
+      const { props } = codeSnippet.props.children;
+      let filePath: string;
+      let fileHidden = false;
+      let fileActive = false;
+
+      if (props.metastring) {
+        const [name, ...params] = props.metastring.split(' ');
+        filePath = '/' + name;
+        if (params.includes('hidden')) {
+          fileHidden = true;
+        }
+        if (params.includes('active')) {
+          fileActive = true;
+        }
+        // isSingleFile = false;
+      } else {
+        if (props.className === 'language-tsx') {
+          filePath = '/App.tsx';
+        } else if (props.className === 'language-js') {
+          filePath = '/App.js';
+        } else if (props.className === 'language-css') {
+          filePath = '/styles.css';
+        } else {
+          throw new Error(`Code block is missing a filename: ${props.children}`);
+        }
+      }
+      if (result[filePath]) {
+        throw new Error(
+          `File ${filePath} was defined multiple times. Each file snippet should have a unique path name.`
+        );
+      }
+      if (filePath === '/styles.css' && !fileHidden) {
+        hideStylesCSS = false;
+      }
+      result[filePath] = {
+        code: props.children,
+        hidden: fileHidden,
+        active: fileActive,
+      };
+
+      return result;
+    },
+    {}
+  );
+
+  const rqbCSSimport = RegExp(`^import +'react-querybuilder/dist/query-builder\\.s?css';?$`).test(
+    files['/App.tsx'].code
+  )
+    ? ''
+    : `@import 'react-querybuilder/dist/query-builder.css';`;
+  const sandboxStyle = `body { background-color: ${bkgdColor}; }`;
+
+  files['/styles.css'] = {
+    code: [rqbCSSimport, sandboxStyle, files['/styles.css']?.code ?? ''].join('\n\n'),
+    hidden: hideStylesCSS,
+  };
+
+  const setup = {
+    ...customSetup,
+    dependencies: { ...customSetup?.dependencies, 'react-querybuilder': 'latest' },
+  };
 
   return (
     <div className="sandpackrqb">
       <Sandpack
-        files={{
-          '/App.tsx': appCode,
-          '/styles.css': {
-            code: `body{background-color:${bkgdColor};}`,
-            hidden: true,
-          },
-        }}
+        files={files}
         theme={isDarkTheme ? 'monokai-pro' : undefined}
         template="react-ts"
-        customSetup={{
-          dependencies: {
-            'react-querybuilder': '4.0.0',
-          },
-        }}
+        customSetup={setup}
+        options={options}
       />
     </div>
   );
