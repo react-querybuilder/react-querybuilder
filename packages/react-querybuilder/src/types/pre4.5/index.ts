4577eef29e16ca42a0be3c1e66b309763dbc0378
Jake Boone <jakeboone02@gmail.com>
2021-12-31 18:36:11 -0700

    Fix issue with last commit

--- packages/react-querybuilder/src/types/pre4.5/index.ts
+++ packages/react-querybuilder/src/types/pre4.5/index.ts
@@ -4,9 +4,10 @@ export type { ValidationResult, ValidationMap, QueryValidator, RuleValidator };
 
 import type { CommonSubComponentProps, ActionProps, ActionWithRulesProps, SelectorEditorProps, ValueSelectorProps, NotToggleProps, CombinatorSelectorProps, FieldSelectorProps, OperatorSelectorProps, ValueEditorProps, DragHandleProps, Controls, Classnames, Schema, Translations, RuleGroupProps, RuleProps, QueryBuilderProps, QueryBuilderPropsInternal } from './props';
 export type { CommonSubComponentProps, ActionProps, ActionWithRulesProps, SelectorEditorProps, ValueSelectorProps, NotToggleProps, CombinatorSelectorProps, FieldSelectorProps, OperatorSelectorProps, ValueEditorProps, DragHandleProps, Controls, Classnames, Schema, Translations, RuleGroupProps, RuleProps, QueryBuilderProps, QueryBuilderPropsInternal };
+
+export * from './basic';
 end temp */
 
 export * from './ruleGroupsIC';
-export * from '../basic';
 export * from '../importExport';
 export * from '../ruleGroups';
