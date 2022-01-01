7ffca6e78404ef1190dd6f24e52de1fdf5e5747d
Jake Boone <jakeboone02@gmail.com>
2021-12-31 22:50:32 -0700

    Remove link to 4.5 features from basic.ts for real this time

--- packages/react-querybuilder/src/types/pre4.5/index.ts
+++ packages/react-querybuilder/src/types/pre4.5/index.ts
@@ -1,13 +1,9 @@
 /* begin temp
-import type { ValidationResult, ValidationMap, QueryValidator, RuleValidator } from './validation';
-export type { ValidationResult, ValidationMap, QueryValidator, RuleValidator };
-
-import type { CommonSubComponentProps, ActionProps, ActionWithRulesProps, SelectorEditorProps, ValueSelectorProps, NotToggleProps, CombinatorSelectorProps, FieldSelectorProps, OperatorSelectorProps, ValueEditorProps, DragHandleProps, Controls, Classnames, Schema, Translations, RuleGroupProps, RuleProps, QueryBuilderProps, QueryBuilderPropsInternal } from './props';
-export type { CommonSubComponentProps, ActionProps, ActionWithRulesProps, SelectorEditorProps, ValueSelectorProps, NotToggleProps, CombinatorSelectorProps, FieldSelectorProps, OperatorSelectorProps, ValueEditorProps, DragHandleProps, Controls, Classnames, Schema, Translations, RuleGroupProps, RuleProps, QueryBuilderProps, QueryBuilderPropsInternal };
-
 export * from './basic';
+export * from './importExport';
+export * from './props';
+export * from './validation';
 end temp */
 
 export * from './ruleGroupsIC';
-export * from '../importExport';
 export * from '../ruleGroups';
