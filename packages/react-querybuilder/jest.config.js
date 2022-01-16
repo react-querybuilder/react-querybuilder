a6798fb4c60172c4950b078d9cf169f759d9540e
Jake Boone <jakeboone02@gmail.com>
2022-01-16 12:34:43 -0700

    Fix jest module mapping config for new tsconfig

--- packages/react-querybuilder/jest.config.js
+++ packages/react-querybuilder/jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   coveragePathIgnorePatterns: ['sqlParser.js'],
-  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^ruleGroupsIC$': '<rootDir>/src/types/ruleGroupsIC.ts',
+  },
   setupFilesAfterEnv: ['../../jestSetup.ts'],
+  testEnvironment: 'jsdom',
 };
