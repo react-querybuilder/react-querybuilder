20bfba0a0ca2299942cf75b95a029e25ebc4b25e
Jake Boone <jakeboone02@gmail.com>
2022-02-18 09:18:00 -0700

    Stabilize AntD optgroup test

--- packages/antd/src/AntD.test.tsx
+++ packages/antd/src/AntD.test.tsx
@@ -115,8 +115,7 @@ const testAntDValueSelector = (
         await new Promise(r => setTimeout(r, 500));
       });
       const listbox = within(getByRole('listbox'));
-      expect(listbox.getAllByRole('option')).toHaveLength(2);
-      expect(listbox.getAllByRole('option')[1]).toHaveTextContent(testVal.name);
+      expect(listbox.getAllByRole('option').pop()).toHaveTextContent(testVal.name);
     });
 
     it('should be disabled by the disabled prop', () => {
