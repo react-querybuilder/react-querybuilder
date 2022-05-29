51201739bfc1304f832b91ce050ddc1d20f65dc7
Jake Boone <jakeboone02@gmail.com>
2022-05-29 13:58:26 -0700

    Reinstate export of isRuleGroup and remove deprecation notice

--- packages/react-querybuilder/src/utils/isRuleGroup.ts
+++ packages/react-querybuilder/src/utils/isRuleGroup.ts
@@ -1,6 +1,10 @@
 import type { RuleGroupType, RuleGroupTypeAny, RuleGroupTypeIC, RuleType } from '../types';
 
-const isRuleGroup = (rg: RuleType | RuleGroupTypeAny): rg is RuleGroupTypeAny =>
+/**
+ * Determines if this is either a RuleGroupType or RuleGroupTypeIC.
+ * `'rules' in query` can be used as an alternative.
+ */
+export const isRuleGroup = (rg: RuleType | RuleGroupTypeAny): rg is RuleGroupTypeAny =>
   typeof rg === 'object' && 'rules' in rg && Array.isArray(rg.rules);
 
 export const isRuleGroupType = (rg: RuleType | RuleGroupTypeAny): rg is RuleGroupType =>
