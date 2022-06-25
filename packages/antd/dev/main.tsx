305ae9ce2892bee33d42b873b2c5c5b5254fdc3d
Jake Boone <jakeboone02@gmail.com>
2022-06-25 00:27:10 -0700

    Add examples to main prettier config

--- packages/antd/dev/main.tsx
+++ packages/antd/dev/main.tsx
@@ -1,8 +1,8 @@
+import 'antd/dist/antd.compact.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from 'react-querybuilder/dev';
 import { antdControlElements } from '../src';
-import 'antd/dist/antd.compact.css';
 import './styles.scss';
 
 createRoot(document.getElementById('app')!).render(
