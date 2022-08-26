702344dbde17a75fce785cc1aa7350e1c32bede7
Jake Boone <jakeboone02@gmail.com>
2022-08-26 09:41:17 -0700

    Dynamicly import MUI in MaterialActionElement

--- packages/material/src/MaterialActionElement.tsx
+++ packages/material/src/MaterialActionElement.tsx
@@ -1,33 +1,89 @@
-import Button from '@mui/material/Button';
-import { ThemeProvider, useTheme } from '@mui/material/styles';
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+import type Button from '@mui/material/Button';
 import type { ComponentPropsWithoutRef } from 'react';
-import type { ActionWithRulesProps } from 'react-querybuilder';
+import { useEffect, useState } from 'react';
+import { ActionElement, ActionWithRulesProps } from 'react-querybuilder';
+import { errorMaterialWithoutMUI } from './messages';
 
 type MaterialActionProps = ActionWithRulesProps & ComponentPropsWithoutRef<typeof Button>;
 
-export const MaterialActionElement = ({
-  className,
-  handleOnClick,
-  label,
-  title,
-  disabled,
-  disabledTranslation,
-  // Props that should not be in extraProps
-  testID: _testID,
-  rules: _rules,
-  level: _level,
-  path: _path,
-  context: _context,
-  validation: _validation,
-  ...extraProps
-}: MaterialActionProps) => {
-  const theme = useTheme();
+type GetMaterialActionElementProps = {
+  components: { Button: typeof Button };
+};
 
-  return (
-    <ThemeProvider theme={theme}>
+let didWarnMaterialWithoutMUI = false;
+
+const useMUI = () => {
+  const [mui, setMUI] = useState<GetMaterialActionElementProps | null>(null);
+
+  useEffect(() => {
+    let didCancel = false;
+
+    const getHook = async () => {
+      const [muiButton] = await Promise.all([import('@mui/material/Button').catch(() => null)]);
+
+      // istanbul ignore else
+      if (!didCancel) {
+        if (muiButton) {
+          const Button = muiButton?.default;
+          setMUI(() => ({ components: { Button } }));
+        } else {
+          // istanbul ignore else
+          if (__RQB_DEV__ && !didWarnMaterialWithoutMUI) {
+            console.error(errorMaterialWithoutMUI);
+            didWarnMaterialWithoutMUI = true;
+          }
+        }
+      }
+    };
+
+    if (!mui) {
+      getHook();
+    }
+
+    return () => {
+      didCancel = true;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return mui;
+};
+
+export const MaterialActionElement = (props: MaterialActionProps) => {
+  const mui = useMUI();
+  const key = mui ? 'theme' : 'no-theme';
+  if (!mui) {
+    return <ActionElement key={key} {...props} />;
+  }
+
+  const Component = getMaterialActionElement(mui);
+
+  return <Component {...props} />;
+};
+
+export const getMaterialActionElement =
+  ({ components: { Button } }: GetMaterialActionElementProps) =>
+  ({
+    className,
+    handleOnClick,
+    label,
+    title,
+    disabled,
+    disabledTranslation,
+    // Props that should not be in extraProps
+    testID: _testID,
+    rules: _rules,
+    level: _level,
+    path: _path,
+    context: _context,
+    validation: _validation,
+    ...extraProps
+  }: MaterialActionProps) =>
+    (
       <Button
         variant="contained"
-        color="primary"
+        color="secondary"
         className={className}
         title={disabledTranslation && disabled ? disabledTranslation.title : title}
         size="small"
@@ -36,8 +92,6 @@ export const MaterialActionElement = ({
         {...extraProps}>
         {disabledTranslation && disabled ? disabledTranslation.label : label}
       </Button>
-    </ThemeProvider>
-  );
-};
+    );
 
 MaterialActionElement.displayName = 'MaterialActionElement';
---
+++ packages/material/src/messages.ts
@@ -0,0 +1,3 @@
+export const errorMaterialWithoutMUI =
+  'MaterialActionElement was rendered without @mui/material ' +
+  'installed. Install @mui/material for this component to work properly.';
