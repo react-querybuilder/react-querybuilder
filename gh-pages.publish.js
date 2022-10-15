69aedb190a0e5557a1af24ae5047bfccda8328e2
Jake Boone <jakeboone02@gmail.com>
2022-10-15 13:25:51 -0700

    Remove unnecessary files

--- gh-pages.publish.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-var ghpages = require('gh-pages');
-
-ghpages.publish('packages/demo/dist', function (err) {
-  if (err) {
-    console.log('Failed to publish');
-  }
-});
