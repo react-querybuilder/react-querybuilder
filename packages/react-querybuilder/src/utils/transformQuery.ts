c5ea6fdd07ace8c7b7748676f2e376c45015eabd
Jake Boone <jakeboone02@gmail.com>
2022-12-06 09:53:09 -0700

    Add Intellisense comments to transformQuery

--- packages/react-querybuilder/src/utils/transformQuery.ts
+++ packages/react-querybuilder/src/utils/transformQuery.ts
@@ -1,19 +1,94 @@
 import type { RuleGroupTypeAny, RuleType } from '@react-querybuilder/ts/src/index.noReact';
 import produce from 'immer';
 
+const remapProperties = (
+  obj: Record<string, any>,
+  propertyMap: Record<string, string>,
+  deleteRemappedProperties: boolean
+) =>
+  produce(obj, draft => {
+    for (const [k, v] of Object.entries(propertyMap)) {
+      draft[v] = draft[k];
+      if (deleteRemappedProperties) {
+        delete draft[k];
+      }
+    }
+  });
+
 interface QueryTransformerOptions {
-  ruleProcessor: (rule: RuleType) => any;
-  ruleGroupProcessor: (ruleGroup: RuleGroupTypeAny) => any;
-  propertyMap: Record<string, string>;
-  combinatorMap: Record<string, string>;
-  operatorMap: Record<string, string>;
-  deleteRemappedProperties: boolean;
+  /**
+   * When a rule is encountered in the hierarchy, it will be replaced
+   * with the result of this function.
+   */
+  ruleProcessor?: (rule: RuleType) => any;
+  /**
+   * When a group is encountered in the hierarchy, it will be replaced
+   * with the result of this function.
+   */
+  ruleGroupProcessor?: (ruleGroup: RuleGroupTypeAny) => any;
+  /**
+   * For each rule and group in the hierarchy, any properties matching a key
+   * in this object will be copied to a property with the corresponding value
+   * as its name.
+   *
+   * @example
+   *   transformQuery(
+   *     { rules: [] },
+   *     { propertyMap: { rules: 'newRules' } }
+   *   )
+   *   // Returns: { rules: [], newRules: [] }
+   */
+  propertyMap?: Record<string, string>;
+  /**
+   * Any combinator values (including independent combinators) will be translated
+   * from the key in this object to the value.
+   *
+   * @example
+   *   transformQuery(
+   *     { combinator: 'and', rules: [] },
+   *     { combinatorMap: { and: 'AND' } }
+   *   )
+   *   // Returns: { combinator: 'AND', rules: [] }
+   */
+  combinatorMap?: Record<string, string>;
+  /**
+   * Any operator values will be translated from the key in this object to the value.
+   *
+   * @example
+   *   transformQuery(
+   *     { combinator: 'and', rules: [{ field: 'name', operator: '=', value: 'Steve Vai' }] },
+   *     { operatorMap: { '=': 'is' } }
+   *   )
+   *   // Returns:
+   *   // {
+   *   //   combinator: 'and',
+   *   //   rules: [{ field: 'name', operator: 'is', value: 'Steve Vai' }]
+   *   // }
+   */
+  operatorMap?: Record<string, string>;
+  /**
+   * Original properties remapped according to the `propertyMap` option will be removed.
+   *
+   * @example
+   *   transformQuery(
+   *     { rules: [] },
+   *     { propertyMap: { rules: 'newRules' }, deleteRemappedProperties: true }
+   *   )
+   *   // Returns: { newRules: [] }
+   */
+  deleteRemappedProperties?: boolean;
 }
 
-export const transformQuery = (
-  query: RuleGroupTypeAny,
-  options: Partial<QueryTransformerOptions> = {}
-) => {
+/**
+ * A versatile utility function to recursively process a query heirarchy.
+ *
+ * Documentation: https://react-querybuilder.js.org/docs/api/misc#transformquery
+ *
+ * @param query The query to transform
+ * @param options
+ * @returns The transformed query
+ */
+export const transformQuery = (query: RuleGroupTypeAny, options: QueryTransformerOptions = {}) => {
   const {
     ruleProcessor = r => r,
     ruleGroupProcessor = rg => rg,
@@ -23,24 +98,18 @@ export const transformQuery = (
     deleteRemappedProperties = true,
   } = options;
 
-  const remapProperties = (obj: Record<string, any>) =>
-    produce(obj, draft => {
-      for (const [k, v] of Object.entries(propertyMap)) {
-        draft[v] = draft[k];
-        if (deleteRemappedProperties) {
-          delete draft[k];
-        }
-      }
-    });
-
   const processGroup = (rg: RuleGroupTypeAny & { path: number[] }): any => ({
     ...ruleGroupProcessor(
-      remapProperties({
-        ...rg,
-        ...('combinator' in rg
-          ? { combinator: combinatorMap[rg.combinator] ?? rg.combinator }
-          : {}),
-      }) as RuleGroupTypeAny
+      remapProperties(
+        {
+          ...rg,
+          ...('combinator' in rg
+            ? { combinator: combinatorMap[rg.combinator] ?? rg.combinator }
+            : {}),
+        },
+        propertyMap,
+        deleteRemappedProperties
+      ) as RuleGroupTypeAny
     ),
     rules: rg.rules.map((r: any, idx) => {
       if (typeof r === 'string') {
@@ -52,10 +121,14 @@ export const transformQuery = (
       }
       // rules
       return ruleProcessor(
-        remapProperties({
-          ...{ ...r, path: [...rg.path, idx] },
-          operator: operatorMap[r.operator] ?? r.operator,
-        }) as RuleType
+        remapProperties(
+          {
+            ...{ ...r, path: [...rg.path, idx] },
+            operator: operatorMap[r.operator] ?? r.operator,
+          },
+          propertyMap,
+          deleteRemappedProperties
+        ) as RuleType
       );
     }),
   });
