02deb34bc1947929bf55c0748adf9a2763b25426
Jake Boone <jakeboone02@gmail.com>
2022-12-07 15:01:20 -0700

    Demo links refresh page

---
+++ website/src/components/DemoLink.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const DemoLink = ({
+  option,
+  disabled = false,
+  text,
+}: {
+  option: string;
+  disabled?: boolean;
+  text?: string;
+}) => (
+  <a href={`/demo#${option}=${!disabled}`}>
+    {text ? text : `Click here for demo${disabled ? ' with this feature disabled' : ''}`}
+  </a>
+);
