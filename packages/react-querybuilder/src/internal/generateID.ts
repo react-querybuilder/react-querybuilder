a9a5b4f2d2bd5775a18ac1bb1baf4f7ae7e7b3b5
Jake Boone <jakeboone02@gmail.com>
2022-12-12 17:44:35 -0700

    Ensure generateID works in Node and browsers
    Fixes #416

--- packages/react-querybuilder/src/internal/generateID.ts
+++ packages/react-querybuilder/src/internal/generateID.ts
@@ -1,1 +1,8 @@
-export const generateID = () => crypto.randomUUID();
+let cryptoModule = globalThis.crypto;
+
+// istanbul ignore next
+if (!cryptoModule && typeof require === 'function') {
+  cryptoModule = require('node:crypto');
+}
+
+export const generateID = () => cryptoModule.randomUUID();
