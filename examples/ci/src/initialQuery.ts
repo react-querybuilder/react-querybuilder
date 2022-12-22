27208d80195ea3032cad8aeaebccfd0d89db9727
Jake Boone <jakeboone02@gmail.com>
2022-12-22 12:04:05 -0700

    Fix listsAsArrays and parseNumbers in CI example

--- examples/ci/src/initialQuery.ts
+++ examples/ci/src/initialQuery.ts
@@ -1,6 +1,6 @@
 import { parseSQL } from 'react-querybuilder';
 
-const sql = `(firstName like 'Stev%' and lastName in ('Vai', 'Vaughan') and age > '28' and (isMusician = TRUE or instrument = 'Guitar'))`;
+const sql = `(firstName like 'Stev%' and lastName in ('Vai', 'Vaughan') and age between 28 and 78 and (isMusician = TRUE or instrument = 'Guitar'))`;
 
 export const initialQuery = parseSQL(sql);
 export const initialQueryIC = parseSQL(sql, { independentCombinators: true });
