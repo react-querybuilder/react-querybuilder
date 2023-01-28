205c426d74cdce109ae4501ca17756d144dd6182
Jake Boone <jakeboone02@gmail.com>
2023-01-28 10:04:47 -0700

    Merge native-base example into native example

--- examples/native-base/README.md
+++
@@ -1,9 +0,0 @@
-## React Query Builder NativeBase Example
-
-[![Open in CodeSandbox](https://img.shields.io/badge/Open_in-CodeSandbox-000000?logo=codesandbox)](https://codesandbox.io/s/github/react-querybuilder/react-querybuilder/tree/main/examples/native-base?file=/src/App.tsx)
-
-[![Open in StackBlitz](https://img.shields.io/badge/Open_in-StackBlitz-1269D3?logo=stackblitz)](https://stackblitz.com/github/react-querybuilder/react-querybuilder/tree/main/examples/native-base?file=src/App.tsx)
-
-Click one of the links above to open this example as an online, editable demo.
-
-This example depends on [`react-native-web`](https://www.npmjs.com/package/react-native-web) for compatibility with browsers and uses components from [`native-base`](https://www.npmjs.com/package/native-base).
--- examples/native/src/App.tsx
+++ examples/native/src/App.tsx
@@ -1,11 +1,20 @@
 import {
   defaultNativeWebControlElements,
+  NativeValueSelector,
+  NativeValueSelectorWeb,
   QueryBuilderNative,
 } from '@react-querybuilder/native';
-import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import type { Field, RuleGroupType } from 'react-querybuilder';
+import { extendTheme, NativeBaseProvider } from 'native-base';
+import { useMemo, useState } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import type {
+  Field,
+  Option,
+  OptionList,
+  RuleGroupType,
+} from 'react-querybuilder';
 import { formatQuery } from 'react-querybuilder';
+import { nativeBaseControlElements } from './components';
 
 const fields: Field[] = [
   { name: 'firstName', label: 'First Name' },
@@ -20,20 +29,74 @@ const defaultQuery: RuleGroupType = {
   ],
 };
 
+type UILib =
+  | 'default'
+  | 'native-base'
+  | 'fluentui'
+  | 'kitten'
+  | 'paper'
+  | 'tamagui';
+
+const uiLibs: OptionList<Option<UILib>> = [
+  { name: 'default', label: 'Default' },
+  { name: 'paper', label: 'React Native Paper' },
+  { name: 'native-base', label: 'NativeBase' },
+  { name: 'fluentui', label: 'Fluent UI' },
+  { name: 'kitten', label: 'Kitten' },
+  { name: 'tamagui', label: 'Tamagui' },
+];
+
+const nativeBaseTheme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+  },
+});
+
+const LibSelector = ({ libSetter }: { libSetter: (lib: UILib) => void }) => {
+  const ValueSelectorComponent =
+    Platform.OS === 'web' ? NativeValueSelectorWeb : NativeValueSelector;
+  return (
+    <ValueSelectorComponent
+      options={uiLibs}
+      handleOnChange={v => libSetter(v)}
+      path={[]}
+      level={0}
+      schema={{} as any}
+    />
+  );
+};
+
 export const App = () => {
+  const [uiLib, setUIlib] = useState<UILib>('default');
   const [query, setQuery] = useState(defaultQuery);
+  const controlElements = useMemo(() => {
+    switch (uiLib) {
+      case 'native-base':
+        return nativeBaseControlElements;
+      default:
+        return defaultNativeWebControlElements;
+    }
+  }, [uiLib]);
 
   return (
-    <View style={styles.outer}>
-      <Text style={styles.outer}>React Query Builder Native Example</Text>
-      <QueryBuilderNative
-        fields={fields}
-        query={query}
-        onQueryChange={q => setQuery(q)}
-        controlElements={defaultNativeWebControlElements}
-      />
-      <Text style={styles.code}>{formatQuery(query, 'sql')}</Text>
-    </View>
+    <NativeBaseProvider theme={nativeBaseTheme}>
+      <View style={styles.outer}>
+        <Text style={styles.outer}>
+          React Query Builder React Native Example
+        </Text>
+        <View>
+          <Text>UI library</Text>
+          <LibSelector libSetter={setUIlib} />
+        </View>
+        <QueryBuilderNative
+          fields={fields}
+          query={query}
+          onQueryChange={q => setQuery(q)}
+          controlElements={controlElements}
+        />
+        <Text style={styles.code}>{formatQuery(query, 'sql')}</Text>
+      </View>
+    </NativeBaseProvider>
   );
 };
 
