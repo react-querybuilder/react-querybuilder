9acf8c8d01a893efee05ed30cbebc167e78a67bf
Jake Boone <jakeboone02@gmail.com>
2023-02-13 16:23:48 -0700

    Attempt to move everything to the client in Mantine demo

--- website/src/pages/demo/mantine.tsx
+++ website/src/pages/demo/mantine.tsx
@@ -1,19 +1,25 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import BrowserOnly from '@docusaurus/BrowserOnly';
-import { QueryBuilderMantine } from '@react-querybuilder/mantine';
 import Layout from '@theme/Layout';
 import React from 'react';
 import './_styles/demo.scss';
 import './_styles/rqb-mantine.scss';
 
 function ReactQueryBuilderDemo_MantineBrowser() {
+  require('@mantine/hooks');
+  const { MantineProvider }: typeof import('@mantine/core') = require('@mantine/core');
+  const {
+    QueryBuilderMantine,
+  }: typeof import('@react-querybuilder/mantine') = require('@react-querybuilder/mantine');
   const Demo: typeof import('./_components/Demo').default = require('./_components/Demo').default;
 
   return (
-    <QueryBuilderMantine>
-      <Demo variant="mantine" />
-    </QueryBuilderMantine>
+    <MantineProvider>
+      <QueryBuilderMantine>
+        <Demo variant="mantine" />
+      </QueryBuilderMantine>
+    </MantineProvider>
   );
 }
 
