b6013f193d9f3860cf6b1c6f7fa50c2b9807c9a2
Jake Boone <jakeboone02@gmail.com>
2023-02-27 12:06:50 -0700

    More tests working in Vitest

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,19 @@
 import matchers from '@testing-library/jest-dom/matchers';
+import '@testing-library/jest-native/extend-expect';
 import { expect } from 'vitest';
 
 expect.extend(matchers);
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
