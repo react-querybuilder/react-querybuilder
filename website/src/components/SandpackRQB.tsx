adb4356766eeb7a95990a219670f433f192461b0
Jake Boone <jakeboone02@gmail.com>
2023-03-05 23:07:41 -0700

    Update website RQB to 6.0.4

--- website/src/components/SandpackRQB.tsx
+++ website/src/components/SandpackRQB.tsx
@@ -105,12 +105,12 @@ h1, h2, h3, h4, h5, h6 {
       ? { 'react-querybuilder': '^4.5.3' }
       : rqbVersion === 5
       ? {
-          '@react-querybuilder/dnd': '^5.3.3',
-          'react-querybuilder': '^5.3.3',
+          '@react-querybuilder/dnd': '^5.4.1',
+          'react-querybuilder': '^5.4.1',
         }
       : {
-          '@react-querybuilder/dnd': '6.0.1',
-          'react-querybuilder': '6.0.1',
+          '@react-querybuilder/dnd': '6.0.4',
+          'react-querybuilder': '6.0.4',
         };
 
   const setup = {
