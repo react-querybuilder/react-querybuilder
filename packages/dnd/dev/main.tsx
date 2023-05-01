2ffbb500a6998e0f00462b543730c64710c700db
Jake Boone <jakeboone02@gmail.com>
2023-05-01 13:40:44 -0700

    Remove ReactDnD preload from dnd package dev page

--- packages/dnd/dev/main.tsx
+++ packages/dnd/dev/main.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
-import * as ReactDnD from 'react-dnd';
-import * as ReactDndHtml5Backend from 'react-dnd-html5-backend';
 import { createRoot } from 'react-dom/client';
 import { App } from 'react-querybuilder/dev';
 import { QueryBuilderDnD } from '../src/QueryBuilderDnD';
 
 createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
-    <QueryBuilderDnD dnd={{ ...ReactDnD, ...ReactDndHtml5Backend }}>
+    <QueryBuilderDnD>
       <App enableDragAndDrop />
     </QueryBuilderDnD>
   </React.StrictMode>
