0d3ec416779f4c580924665c755ca3ffe9f378c8
Jake Boone <jakeboone02@gmail.com>
2023-05-01 13:40:13 -0700

    Remove Vite build configurations

--- packages/react-querybuilder/vite.config.ts
+++ packages/react-querybuilder/vite.config.ts
@@ -1,42 +1,9 @@
 import vitePluginReact from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { visualizer } from 'rollup-plugin-visualizer';
-import type { PluginOption } from 'vite';
 import { defineConfig } from 'vite';
-import { name } from './package.json';
 
-export default defineConfig(({ mode }) => ({
-  build: {
-    emptyOutDir: mode === 'production',
-    minify: mode === 'production',
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      fileName: format =>
-        `index${mode === 'production' ? '' : '.development'}.${format === 'es' ? 'm' : 'c'}js`,
-      formats: ['cjs', 'es'],
-      name: 'ReactQueryBuilder',
-    },
-    rollupOptions: {
-      external: ['immer', 'react', 'react/jsx-runtime'],
-      output: {
-        globals: { immer: 'immer', react: 'React' },
-        exports: 'named',
-      },
-    },
-    sourcemap: true,
-    target: 'es2020',
-  },
-  plugins: [
-    vitePluginReact(),
-    visualizer(_opts => ({
-      // filename: `build-stats.${_opts.format}.html`,
-      filename: `build-stats.html`,
-      gzipSize: true,
-      brotliSize: true,
-      title: `Build stats (${name})`,
-    })) as PluginOption,
-  ],
+export default defineConfig({
+  plugins: [vitePluginReact()],
   server: {
     port: 3100,
   },
-}));
+});
