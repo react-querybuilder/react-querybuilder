b9bf29832c942bbfdc87e5b7df95fcfcc6772fbe
Jake Boone <jakeboone02@gmail.com>
2023-10-26 23:12:38 -0700

    Speed up usePreferProp tests

--- packages/react-querybuilder/src/hooks/usePreferProp.test.ts
+++ packages/react-querybuilder/src/hooks/usePreferProp.test.ts
@@ -1,70 +1,67 @@
-import { renderHook } from '@testing-library/react';
 import { usePreferAnyProp, usePreferProp } from './usePreferProp';
+
+jest.mock('react', () => ({ useMemo: (fn: () => any) => fn() }));
 
 describe('usePreferProp', () => {
   it('prefers the prop with default true', async () => {
-    expect(renderHook(() => usePreferProp(true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferProp(true, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferProp(true, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferProp(true, undefined, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferProp(true, undefined, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferProp(true, true, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferProp(true, false, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferProp(true, false, true)).result.current).toBe(false);
-    expect(renderHook(() => usePreferProp(true, true, false)).result.current).toBe(true);
+    expect(usePreferProp(true)).toBe(true);
+    expect(usePreferProp(true, true)).toBe(true);
+    expect(usePreferProp(true, false)).toBe(false);
+    expect(usePreferProp(true, undefined, true)).toBe(true);
+    expect(usePreferProp(true, undefined, false)).toBe(false);
+    expect(usePreferProp(true, true, true)).toBe(true);
+    expect(usePreferProp(true, false, false)).toBe(false);
+    expect(usePreferProp(true, false, true)).toBe(false);
+    expect(usePreferProp(true, true, false)).toBe(true);
   });
 
   it('prefers the prop with default false', async () => {
-    expect(renderHook(() => usePreferProp(false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferProp(false, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferProp(false, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferProp(false, undefined, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferProp(false, undefined, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferProp(false, true, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferProp(false, false, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferProp(false, false, true)).result.current).toBe(false);
-    expect(renderHook(() => usePreferProp(false, true, false)).result.current).toBe(true);
+    expect(usePreferProp(false)).toBe(false);
+    expect(usePreferProp(false, true)).toBe(true);
+    expect(usePreferProp(false, false)).toBe(false);
+    expect(usePreferProp(false, undefined, true)).toBe(true);
+    expect(usePreferProp(false, undefined, false)).toBe(false);
+    expect(usePreferProp(false, true, true)).toBe(true);
+    expect(usePreferProp(false, false, false)).toBe(false);
+    expect(usePreferProp(false, false, true)).toBe(false);
+    expect(usePreferProp(false, true, false)).toBe(true);
   });
 });
 
 describe('usePreferAnyProp', () => {
   it('prefers the prop with default undefined', async () => {
-    expect(renderHook(() => usePreferAnyProp(undefined)).result.current).toBeUndefined();
-    expect(renderHook(() => usePreferAnyProp(undefined, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferAnyProp(undefined, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferAnyProp(undefined, undefined, true)).result.current).toBe(
-      true
-    );
-    expect(renderHook(() => usePreferAnyProp(undefined, undefined, false)).result.current).toBe(
-      false
-    );
-    expect(renderHook(() => usePreferAnyProp(undefined, true, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferAnyProp(undefined, false, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferAnyProp(undefined, false, true)).result.current).toBe(false);
-    expect(renderHook(() => usePreferAnyProp(undefined, true, false)).result.current).toBe(true);
+    expect(usePreferAnyProp(undefined)).toBeUndefined();
+    expect(usePreferAnyProp(undefined, true)).toBe(true);
+    expect(usePreferAnyProp(undefined, false)).toBe(false);
+    expect(usePreferAnyProp(undefined, undefined, true)).toBe(true);
+    expect(usePreferAnyProp(undefined, undefined, false)).toBe(false);
+    expect(usePreferAnyProp(undefined, true, true)).toBe(true);
+    expect(usePreferAnyProp(undefined, false, false)).toBe(false);
+    expect(usePreferAnyProp(undefined, false, true)).toBe(false);
+    expect(usePreferAnyProp(undefined, true, false)).toBe(true);
   });
 
   it('prefers the prop with default true', async () => {
-    expect(renderHook(() => usePreferAnyProp(true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferAnyProp(true, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferAnyProp(true, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferAnyProp(true, undefined, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferAnyProp(true, undefined, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferAnyProp(true, true, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferAnyProp(true, false, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferAnyProp(true, false, true)).result.current).toBe(false);
-    expect(renderHook(() => usePreferAnyProp(true, true, false)).result.current).toBe(true);
+    expect(usePreferAnyProp(true)).toBe(true);
+    expect(usePreferAnyProp(true, true)).toBe(true);
+    expect(usePreferAnyProp(true, false)).toBe(false);
+    expect(usePreferAnyProp(true, undefined, true)).toBe(true);
+    expect(usePreferAnyProp(true, undefined, false)).toBe(false);
+    expect(usePreferAnyProp(true, true, true)).toBe(true);
+    expect(usePreferAnyProp(true, false, false)).toBe(false);
+    expect(usePreferAnyProp(true, false, true)).toBe(false);
+    expect(usePreferAnyProp(true, true, false)).toBe(true);
   });
 
   it('prefers the prop with default false', async () => {
-    expect(renderHook(() => usePreferAnyProp(false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferAnyProp(false, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferAnyProp(false, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferAnyProp(false, undefined, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferAnyProp(false, undefined, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferAnyProp(false, true, true)).result.current).toBe(true);
-    expect(renderHook(() => usePreferAnyProp(false, false, false)).result.current).toBe(false);
-    expect(renderHook(() => usePreferAnyProp(false, false, true)).result.current).toBe(false);
-    expect(renderHook(() => usePreferAnyProp(false, true, false)).result.current).toBe(true);
+    expect(usePreferAnyProp(false)).toBe(false);
+    expect(usePreferAnyProp(false, true)).toBe(true);
+    expect(usePreferAnyProp(false, false)).toBe(false);
+    expect(usePreferAnyProp(false, undefined, true)).toBe(true);
+    expect(usePreferAnyProp(false, undefined, false)).toBe(false);
+    expect(usePreferAnyProp(false, true, true)).toBe(true);
+    expect(usePreferAnyProp(false, false, false)).toBe(false);
+    expect(usePreferAnyProp(false, false, true)).toBe(false);
+    expect(usePreferAnyProp(false, true, false)).toBe(true);
   });
 });
