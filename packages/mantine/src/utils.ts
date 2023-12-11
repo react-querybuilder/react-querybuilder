680c9ae2eb20c840e7bc45312d85b82f52e992b9
Dubbo Feng <dubbo@lend.com.au>
2023-12-11 13:16:35 +1100

    add checking mantine version

--- packages/mantine/src/utils.ts
+++ packages/mantine/src/utils.ts
@@ -1,22 +1,47 @@
+import mantineVersion from '@mantine/core/package.json';
 import type { FullOption, FullOptionList, Option } from 'react-querybuilder';
 import { isOptionGroupArray, uniqByIdentifier } from 'react-querybuilder';
 
-export const optionListMapNameToValue = (list: FullOptionList<FullOption>): FullOption[] => {
-  let listFlat: (Option & { group?: string })[] = [];
+export const optionListMapNameToValue = (
+  list: FullOptionList<FullOption>
+): Partial<FullOption>[] => {
+  let listFlat: (Option & { group?: string; items?: Option[] })[] = [];
 
   if (isOptionGroupArray(list)) {
-    listFlat = uniqByIdentifier(
-      listFlat.concat(
-        ...list.map(opt =>
-          opt.options.map(o => ({ name: o.name, value: o.name, label: o.label, group: opt.label }))
+    const mantineMainVersion = parseInt(mantineVersion.version.split('.')[0]);
+    if (mantineMainVersion >= 7) {
+      return list.map(opt => ({
+        group: opt.label,
+        items: opt.options.map(o => ({
+          name: o.name,
+          value: o.name,
+          label: o.label,
+        })),
+      }));
+    } else {
+      listFlat = uniqByIdentifier(
+        listFlat.concat(
+          ...list.map(opt =>
+            opt.options.map(o => ({
+              name: o.name,
+              value: o.name,
+              label: o.label,
+              group: opt.label,
+            }))
+          )
         )
-      )
-    );
+      );
+      return listFlat.map(opt => ({
+        name: opt.name,
+        value: opt.name,
+        label: opt.label,
+        group: opt.group,
+      }));
+    }
   } else {
     listFlat = uniqByIdentifier(list);
+    return listFlat.map(opt => ({ name: opt.name, value: opt.name, label: opt.label }));
   }
-
-  return listFlat.map(opt => ({ name: opt.name, value: opt.name, label: opt.label, group: opt.group}));
 };
 
 export const toNumberInputValue = (val: any) => {
