d6619bc7139ca06e5e1cae9bd2631d9655b1b90e
Jake Boone <jakeboone02@gmail.com>
2024-03-13 14:06:47 -0700

    Fix `window` usage in sandbox redirect

--- website/src/pages/sandbox.tsx
+++ website/src/pages/sandbox.tsx
@@ -1,6 +1,6 @@
 import Layout from '@theme/Layout';
 import queryString from 'query-string';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const csbTemplateLink =
   'https://githubbox.com/react-querybuilder/react-querybuilder/tree/main/examples/_template?file=/src/App.tsx';
@@ -8,21 +8,22 @@ const sbTemplateLink =
   'https://stackblitz.com/github/react-querybuilder/react-querybuilder/tree/main/examples/_template?file=src/App.tsx';
 
 export default function SandboxRedirect() {
-  const query = queryString.parse(window.location.search);
-  const qsTemplate = query.template ?? query.t ?? 'basic-ts';
-  const template = Array.isArray(qsTemplate) ? qsTemplate[0] : qsTemplate;
-  const qsPlatform = query.platform ?? query.p ?? 'csb';
-  const platformPrelim = Array.isArray(qsPlatform) ? qsPlatform[0] : qsPlatform;
-  const platform = ['sb', 'stackblitz'].includes(platformPrelim.toLocaleLowerCase())
-    ? 'StackBlitz'
-    : 'CodeSandbox';
-  const finalLink = (platform === 'StackBlitz' ? sbTemplateLink : csbTemplateLink)
-    .replace(/\b_template\b/, template)
-    .replace(/\/App.tsx\b/, template === 'basic' ? '/App.js' : '/App.tsx');
+  const [{ platform, finalLink }, setLinkInfo] = useState({ platform: '', finalLink: '' });
 
   useEffect(() => {
+    const query = queryString.parse(window.location.search);
+    const qsTemplate = query.template ?? query.t ?? 'basic-ts';
+    const template = Array.isArray(qsTemplate) ? qsTemplate[0] : qsTemplate;
+    const qsPlatform = query.platform ?? query.p ?? 'csb';
+    const platformPrelim = Array.isArray(qsPlatform) ? qsPlatform[0] : qsPlatform;
+    const platform = ['sb', 'stackblitz'].includes(platformPrelim.toLocaleLowerCase())
+      ? 'StackBlitz'
+      : 'CodeSandbox';
+    const finalLink = (platform === 'StackBlitz' ? sbTemplateLink : csbTemplateLink)
+      .replace(/\b_template\b/, template)
+      .replace(/(\/App.tsx\b)/, template === 'basic' ? '/App.js' : '$1');
+    setLinkInfo({ platform, finalLink });
     setTimeout(() => window.location.replace(finalLink), 500);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
