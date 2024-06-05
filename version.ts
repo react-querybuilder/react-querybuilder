8443443edaa12f42fe2e616c3bd5d6495a861a76
Jake Boone <jakeboone02@gmail.com>
2024-06-05 11:52:30 -0700

    Pin versions

--- version.ts
+++ version.ts
@@ -1,23 +1,26 @@
-import { readdir } from 'node:fs/promises';
+export {};
 
 const { version } = await Bun.file('./lerna.json').json();
 
 const packagesDir = `${import.meta.dir}/packages`;
 
-const packages = (await readdir(packagesDir, { withFileTypes: true })).filter(
-  p => p.isDirectory() && p.name !== 'react-querybuilder'
-);
+const pkgJsonPaths = (await Bun.$`ls packages`.text())
+  .split('\n')
+  .filter(Boolean)
+  .filter(p => p !== 'react-querybuilder')
+  .map(p => `${packagesDir}/${p}/package.json`);
 
-await Promise.all(
-  packages.map(async ({ name }) => {
-    const pkgJsonPath = `${packagesDir}/${name}/package.json`;
+pkgJsonPaths.push(`${import.meta.dir}/website/package.json`);
+
+await Promise.all([
+  ...pkgJsonPaths.map(async pkgJsonPath => {
     const pkgJson = await Bun.file(pkgJsonPath).text();
     const replacedRqbDeps = pkgJson.replaceAll(
       /(\s+"react-querybuilder":\s+").+(",?)/g,
-      `$1^${version}$2`
+      `$1${version}$2`
     );
     return Bun.write(pkgJsonPath, replacedRqbDeps);
-  })
-);
+  }),
+]);
 
 console.log('Finished updating local `react-querybuilder` dependency versions.');
