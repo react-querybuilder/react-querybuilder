00b33e79aafd22ba6ecab87e2834c2fc9a2caa32
Jake Boone <jakeboone02@gmail.com>
2024-06-06 09:05:32 -0700

    Remove bunTestSetup.ts

--- bunTestSetup.ts
+++
@@ -1,7 +0,0 @@
-// > bun test --preload bunTestSetup.ts
-
-// import { setAutoFreeze } from 'immer';
-
-// This avoids https://github.com/oven-sh/bun/issues/2417
-// setAutoFreeze(false);
-export {};
