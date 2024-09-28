d75aac4686f1849523d41e68f7f5cea0221408df
Jake Boone <jakeboone02@gmail.com>
2024-09-27 18:02:26 -0700

    Stylistic tweaks to buildCSS.ts

--- packages/react-querybuilder/buildCSS.ts
+++ packages/react-querybuilder/buildCSS.ts
@@ -2,22 +2,26 @@ import { compile } from 'sass';
 
 const src2dist = (path: string) => path.replace(/\bsrc\b/, 'dist/');
 const scss2css = (path: string) => path.replace(/\.scss$/, '.css');
-const full2relativePath = (path: string) => path.replace(/^.*?\/(src\/.*css$)/, '$1');
+const full2relativePath = (path: string) =>
+  path.replace(/^.*?\/(src\/.*css$)/, '$1').replace(/\bsrc\//, './');
 
-// Find all src/*.scss files
 const scssGlob = new Bun.Glob('src/*.scss');
 const scssPaths = scssGlob.scan();
 
-// Write SCSS source, compiled CSS, and source map to dist directory
 for await (const filePath of scssPaths) {
   const bunFile = Bun.file(filePath);
   const fileContent = await bunFile.text();
   const { css, sourceMap } = compile(filePath, { sourceMap: true, style: 'compressed' });
-  sourceMap!.sources = sourceMap!.sources.map(s => full2relativePath(s).replace(/\bsrc\//, './'));
+  sourceMap!.sources = sourceMap!.sources.map(full2relativePath);
   const sourceMapText = JSON.stringify(sourceMap);
+  const scssDistPath = src2dist(filePath);
+  const cssDistPath = src2dist(scss2css(filePath));
   await Promise.all([
-    Bun.write(src2dist(filePath), fileContent),
-    Bun.write(src2dist(scss2css(filePath)), css),
-    Bun.write(`${src2dist(scss2css(filePath))}.map`, sourceMapText),
+    // Copy SCSS source to dist directory
+    Bun.write(scssDistPath, fileContent),
+    // Write compiled CSS to dist directory
+    Bun.write(cssDistPath, css),
+    // Write source map to dist directory
+    Bun.write(`${cssDistPath}.map`, sourceMapText),
   ]);
 }
