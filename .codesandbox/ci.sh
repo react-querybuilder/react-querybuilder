a8b6a788187d61463fb57d9b71e10003bdf93af6
Jake Boone <jakeboone02@gmail.com>
2024-10-08 11:18:40 -0700

    Pin Bun to v1.1.29
    See oven-sh/bun#14414

--- .codesandbox/ci.sh
+++ .codesandbox/ci.sh
@@ -9,7 +9,7 @@
 # an error about not being compatible with Yarn because it sees Yarn's
 # user agent string since CodeSandbox always runs `yarn run ...`.
 # See https://github.com/oven-sh/bun/issues/2530.
-npm_config_user_agent="npm/? node/?" npm install --global bun
+npm_config_user_agent="npm/? node/?" npm install --global bun@1.1.29 # https://github.com/oven-sh/bun/issues/14414
 
 # Now we can get on with business...
 bun install --frozen-lockfile
