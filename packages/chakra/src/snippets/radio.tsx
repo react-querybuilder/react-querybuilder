369debb6b6444af1b18c7252d1f2506cf57e31f5
Jake Boone <jakeboone02@gmail.com>
2024-10-29 10:17:18 -0700

    Temporary types fix for Chakra radio snippet

--- packages/chakra/src/snippets/radio.tsx
+++ packages/chakra/src/snippets/radio.tsx
@@ -2,22 +2,27 @@ import { RadioGroup as ChakraRadioGroup } from '@chakra-ui/react';
 import * as React from 'react';
 
 export interface RadioProps extends ChakraRadioGroup.ItemProps {
-  rootRef?: React.Ref<HTMLDivElement>;
+  rootRef?: React.Ref<HTMLLabelElement>;
   inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
   children?: React.ReactNode;
   value?: string;
 }
+
+// TODO: fix types
+const CRGI = ChakraRadioGroup.Item as unknown as React.ForwardRefExoticComponent<
+  RadioProps & React.RefAttributes<HTMLLabelElement>
+>;
 
 export const Radio: React.ForwardRefExoticComponent<
   RadioProps & React.RefAttributes<HTMLInputElement>
 > = React.forwardRef<HTMLInputElement, RadioProps>(function Radio(props, ref) {
   const { children, inputProps, rootRef, ...rest } = props;
   return (
-    <ChakraRadioGroup.Item ref={rootRef} {...rest}>
+    <CRGI ref={rootRef} {...rest}>
       <ChakraRadioGroup.ItemHiddenInput ref={ref} {...inputProps} />
       <ChakraRadioGroup.ItemIndicator />
       {children && <ChakraRadioGroup.ItemText>{children}</ChakraRadioGroup.ItemText>}
-    </ChakraRadioGroup.Item>
+    </CRGI>
   );
 });
 
