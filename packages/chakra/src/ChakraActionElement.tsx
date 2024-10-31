d00de211f2ae0e10452d0f5e117726468c01e38b
Jake Boone <jakeboone02@gmail.com>
2024-10-31 10:30:56 -0700

    Remove unnecessary Chakra snippets

--- packages/chakra/src/ChakraActionElement.tsx
+++ packages/chakra/src/ChakraActionElement.tsx
@@ -1,7 +1,7 @@
+import { Button } from '@chakra-ui/react';
 import type { ComponentPropsWithoutRef } from 'react';
 import * as React from 'react';
 import type { ActionWithRulesProps } from 'react-querybuilder';
-import { Button } from './snippets/button';
 
 export type ChakraActionProps = ActionWithRulesProps & ComponentPropsWithoutRef<typeof Button>;
 
