2955866b40b5c05bd23f30d1297790678b928abc
Jake Boone <jakeboone02@gmail.com>
2024-12-06 11:44:07 -0700

    Fix logic for when to build/rebuild CEL evaluator

--- packages/react-querybuilder/src/utils/formatQuery/dbquery.cel.test.ts
+++ packages/react-querybuilder/src/utils/formatQuery/dbquery.cel.test.ts
@@ -8,37 +8,57 @@ import { formatQuery } from './formatQuery';
 const skipGoCompiledTestsEnvVar = 'RQB_SKIP_GO_COMPILED_TESTS';
 
 const repoRootDir = path.join(import.meta.dir, '../../../../..');
+const celEvaluatorDir = `${repoRootDir}/utils/cel-evaluator`;
 $.nothrow();
 $.cwd(repoRootDir);
 
-const { exitCode: whichGoExitCode } = await $`go version`.quiet();
-
-if (whichGoExitCode > 0) {
-  // Bail out if Go is not installed
-  test.skip('CEL dbquery tests skipped - Go not installed');
-} else if (process.env[skipGoCompiledTestsEnvVar]) {
+if (process.env[skipGoCompiledTestsEnvVar]) {
   // Bail out if we don't even want to try to run Go compiled tests
   test.skip(`CEL dbquery tests skipped - ${skipGoCompiledTestsEnvVar} env var set`);
 } else {
-  const { mtimeMs: srcLastModified } = await stat(
-    `${repoRootDir}/utils/cel-evaluator/cel-evaluator.go`
-  );
+  // Check if we need to rebuild the CEL evaluator
+  const srcModifiedCheck =
+    await $`git diff --name-only --cached ${celEvaluatorDir}/cel-evaluator.go && git ls-files -m ${celEvaluatorDir}/cel-evaluator.go`.text();
+  const srcModified = !!srcModifiedCheck.trim();
+  const srcLatestCommit =
+    await $`git log -1 --format=%cd --date=unix ${celEvaluatorDir}/cel-evaluator.go`.text();
+  const srcLatestCommitMs = Number(srcLatestCommit) * 1000;
+  const { mtimeMs: srcLastModified } = await stat(`${celEvaluatorDir}/cel-evaluator.go`);
   const { mtimeMs: binLastModified } = await stat(
-    `${repoRootDir}/utils/cel-evaluator/cel-evaluator${process.platform === 'win32' ? '.exe' : ''}`
+    `${celEvaluatorDir}/cel-evaluator${process.platform === 'win32' ? '.exe' : ''}`
   ).catch(() => ({ mtimeMs: 0 }));
 
   let buildInvalid = binLastModified === 0;
-  const buildOutdated = srcLastModified > binLastModified;
+  let buildOutdated =
+    buildInvalid ||
+    // Re: next two lines, `srcLastModified` may be later then `binLastModified`,
+    // but if the source file isn't modified from the last commit then the build
+    // is only outdated if the binary is older than the commit.
+    (srcLastModified > binLastModified && srcModified) ||
+    srcLatestCommitMs > binLastModified;
 
   if (buildInvalid || buildOutdated) {
-    const { exitCode: goBuildExitCode } = await $`go build cel-evaluator.go`.cwd(
-      `${repoRootDir}/utils/cel-evaluator`
-    );
-    buildInvalid = goBuildExitCode > 0;
+    // We need to rebuild the CEL evaluator, so check if Go is installed
+    const { exitCode: goVersionExitCode } = await $`go version`.quiet();
+    if (goVersionExitCode > 0) {
+      // Bail out if Go is not installed
+      test.skip('CEL dbquery tests skipped - Go not installed');
+      buildInvalid = true;
+    } else {
+      // Build CEL evaluator package with Go
+      const { exitCode: goBuildExitCode } = await $`go build cel-evaluator.go`.cwd(
+        `${celEvaluatorDir}`
+      );
+      buildInvalid = goBuildExitCode > 0;
+      buildOutdated = buildOutdated && buildInvalid;
+    }
   }
 
-  if (buildInvalid) {
-    // Bail out if Go build fails
+  if (buildInvalid && buildOutdated) {
+    // CEL evaluator was outdated but rebuild failed
+    test.skip('CEL dbquery tests skipped - CEL evaluator outdated; Go build failed');
+  } else if (buildInvalid) {
+    // Bail out if Go build failed
     test.skip('CEL dbquery tests skipped - Go build failed');
   } else {
     // Run tests
