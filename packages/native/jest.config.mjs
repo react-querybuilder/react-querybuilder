089e803aa7db5d4e10b24003bc973ed0d6a95dd3
Jake Boone <jakeboone02@gmail.com>
2025-01-09 15:44:00 -0700

    Update dependencies

--- packages/native/jest.config.mjs
+++ packages/native/jest.config.mjs
@@ -1,15 +1,15 @@
-import testingLibraryReactNativeJestPreset from '@testing-library/react-native/jest-preset/index.js';
+// @ts-check
 import common from '../../jest.common.mjs';
 
 const { coveragePathIgnorePatterns, globals, moduleNameMapper } = common;
 
 /** @type {import('@jest/types').Config.InitialOptions} */
 export default {
-  preset: '@testing-library/react-native',
+  preset: 'react-native',
   coveragePathIgnorePatterns,
   displayName: 'native',
   globals,
-  setupFilesAfterEnv: [...(testingLibraryReactNativeJestPreset.setupFiles ?? []), './jestSetup.ts'],
+  setupFilesAfterEnv: ['./jestSetup.ts'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|react-redux|@react-native(-community|-picker)?)/)',
   ],
