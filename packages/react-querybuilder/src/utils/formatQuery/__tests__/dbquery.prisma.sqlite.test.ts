cf8a94f52e1b9aa01337df225d9a68d72d52f4dc
Jake Boone <jakeboone02@gmail.com>
2025-05-20 15:08:03 -0700

    Revert "Generate Prisma client outside node_modules"
    This reverts commit 0c7ec2528d24ef29c1e68754d0628594a692a37c.

--- packages/react-querybuilder/src/utils/formatQuery/__tests__/dbquery.prisma.sqlite.test.ts
+++ packages/react-querybuilder/src/utils/formatQuery/__tests__/dbquery.prisma.sqlite.test.ts
@@ -1,5 +1,5 @@
 import { PGlite } from '@electric-sql/pglite';
-import { PrismaClient } from './prisma-client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPGlite } from 'pglite-prisma-adapter';
 import type { TestSQLParams } from '../dbqueryTestUtils';
 import { CREATE_INDEX, CREATE_TABLE, dbTests, fields, superUsers } from '../dbqueryTestUtils';
