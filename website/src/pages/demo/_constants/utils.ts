56325bebea2ca858acbe9af5f51f762416a840d4
Jake Boone <jakeboone02@gmail.com>
2025-06-28 14:04:40 -0700

    Prettier

--- website/src/pages/demo/_constants/utils.ts
+++ website/src/pages/demo/_constants/utils.ts
@@ -83,19 +83,19 @@ const formatQueryCache = new Map();
 
 export const getFormatQueryString = (query: RuleGroupTypeAny, options: FormatQueryOptions) => {
   const cacheKey = JSON.stringify([query, options]);
-  
+
   if (formatQueryCache.has(cacheKey)) {
     return formatQueryCache.get(cacheKey);
   }
 
   const result = formatQueryUncached(query, options);
-  
+
   // Limit cache size to prevent memory leaks
   if (formatQueryCache.size > 50) {
     const firstKey = formatQueryCache.keys().next().value;
     formatQueryCache.delete(firstKey);
   }
-  
+
   formatQueryCache.set(cacheKey, result);
   return result;
 };
