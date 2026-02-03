3057f5d4addb8d57a23590b32ea804f9b8dc7bc9
Jake Boone <jakeboone02@gmail.com>
2026-02-03 08:42:54 -0700

    Update CodeSandbox CI script to avoid Prisma preinstall

--- .codesandbox/ci.sh
+++ .codesandbox/ci.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-# CodeSandbox CI doesn't have the `alias` or `source` commands available,
-# so we can't use the line below (the preferred installation method)...
-# curl -fsSL https://bun.sh/install | bash
-
-# ...we have to use an npm global install:
 npm install --global bun
-# Use this to pin Bun version:
-# npm install --global bun@1.1.29
 
-# Now we can get on with business...
-bun install --frozen-lockfile
+# Ignore scripts to avoid prisma preinstall, which fails because the
+# CodeSandbox CI Node version doesn't meet Prisma's minimum requirement.
+bun install --frozen-lockfile --ignore-scripts
+bun node_modules/bun/install.js
