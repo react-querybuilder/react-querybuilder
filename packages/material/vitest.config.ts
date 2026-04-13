444f5d7830211c106e95413cafd924e31963fd47
Jake Boone <jakeboone02@gmail.com>
2026-04-13 16:03:43 -0700

    Use deep imports for MUI to fix slow tests

--- packages/material/vitest.config.ts
+++ packages/material/vitest.config.ts
@@ -4,5 +4,12 @@ import shared from '../../vitest.shared';
 export default mergeConfig(shared, {
   test: {
     environment: 'jsdom',
+    deps: {
+      optimizer: {
+        web: {
+          include: [/^@mui\//, /^@emotion\//],
+        },
+      },
+    },
   },
 });
