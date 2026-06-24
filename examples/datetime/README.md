51614cbea606ffe47639e34460358337114bd1fc
Jake Boone <jakeboone02@gmail.com>
2026-06-24 12:30:19 -0700

    Add date/time package example

---
+++ examples/datetime/README.md
@@ -0,0 +1,11 @@
+## React Query Builder Date/Time Example
+
+[![Open in CodeSandbox](https://img.shields.io/badge/Open_in-CodeSandbox-000000?logo=codesandbox)](https://react-querybuilder.js.org/sandbox?t=datetime) [![Open in StackBlitz](https://img.shields.io/badge/Open_in-StackBlitz-1269D3?logo=stackblitz)](https://react-querybuilder.js.org/sandbox?p=stackblitz&t=datetime)
+
+Click one of the links above to open this example as an online, editable demo, or clone it locally with [degit](https://www.npmjs.com/package/degit):
+
+```bash
+npx degit github:react-querybuilder/react-querybuilder/examples/datetime rqb-example-datetime
+```
+
+> _Development note: Do not modify the files in this folder directly. Edit corresponding files in the [\_template](../_template) folder and/or [exampleConfigs.ts](../exampleConfigs.ts), then run `bun generate-examples` from the repository root directory (requires [Bun](https://bun.sh/))._
---
+++ examples/datetime/src/App.tsx
@@ -0,0 +1,34 @@
+import { QueryBuilderDateTime } from '@react-querybuilder/datetime/ui';
+import { useState } from 'react';
+import type { Field, RuleGroupType } from 'react-querybuilder';
+import { QueryBuilder, formatQuery } from 'react-querybuilder';
+import './styles.css';
+
+const fields: Field[] = [
+  { name: 'firstName', label: 'First Name' },
+  { name: 'lastName', label: 'Last Name' },
+];
+
+const initialQuery: RuleGroupType = {
+  combinator: 'and',
+  rules: [
+    { field: 'firstName', operator: 'beginsWith', value: 'Stev' },
+    { field: 'lastName', operator: 'in', value: 'Vai,Vaughan' },
+  ],
+};
+
+export const App = () => {
+  const [query, setQuery] = useState(initialQuery);
+
+  return (
+    <div>
+      <QueryBuilderDateTime>
+        <QueryBuilder fields={fields} query={query} onQueryChange={setQuery} />
+      </QueryBuilderDateTime>
+      <h4>Query</h4>
+      <pre>
+        <code>{formatQuery(query, 'json')}</code>
+      </pre>
+    </div>
+  );
+};
