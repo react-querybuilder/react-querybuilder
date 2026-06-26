335ac43018d391322c0972a107864a003493f3e5
Jake Boone <jakeboone02@gmail.com>
2026-06-26 14:06:47 -0700

    Add test scripts for each package for granular coverage checks

--- vitest.config.ts
+++ vitest.config.ts
@@ -56,12 +56,10 @@ export default defineConfig({
         'packages/*/dist/**',
         '**/*TestUtils.ts',
         // Package-specific exclusions
-        'packages/antd/src/dayjs.ts',
         'packages/chakra/src/snippets/**',
-        'packages/dnd/src/isHotkeyPressed.ts',
-        'packages/rules-engine/src/components/**',
         'packages/core/src/utils/parse{CEL,SQL}/{cel,sql}Parser.ts',
         'packages/core/src/utils/parse{Cypher,Gremlin,SPARQL}/parse{Cypher,Gremlin,SPARQL}.ts',
+        'packages/rules-engine/src/components/**',
       ],
       thresholds: { branches: 100, functions: 100, lines: 100, statements: 100 },
     },
