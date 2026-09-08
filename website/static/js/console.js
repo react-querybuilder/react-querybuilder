87940b62bd5a56c500656d48b3c089d4722ce56a
Jake Boone <jakeboone02@gmail.com>
2026-09-08 11:40:52 -0700

    Fade console message

--- website/static/js/console.js
+++ website/static/js/console.js
@@ -2,7 +2,11 @@
 // var process = { env: { NODE_ENV: 'development' } };
 
 const commonStyles = 'font-family: monospace';
+const colorPrimary_n2 = 'color: #d0dbf2';
+const colorPrimary_n1 = 'color: #aabee7';
 const colorPrimary = 'color: #82a7dd';
+const colorPrimary_p1 = 'color: #7395d7';
+const colorPrimary_p2 = 'color: #5e86d1';
 const colorSecondary = 'color: #3c649f';
 const textShadowNone = 'text-shadow: none';
 const textShadow = 'text-shadow: 2px 2px 2px rgba(152, 152, 152, 0.69)';
@@ -18,23 +22,23 @@ const rqb = `%c
 `;
 
 const styleSequence = [
-  [colorPrimary, textShadowNone], // start of line 1
+  [colorPrimary_n2, textShadowNone], // start of line 1
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_n2, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_n2, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_n1, textShadowNone],
   [colorSecondary, textShadowNone], // first shadow in line 2
-  [colorPrimary, textShadowNone],
+  [colorPrimary_n1, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_n1, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_n1, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_n1, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_n1, textShadowNone],
   [colorSecondary, textShadowNone],
   [colorPrimary, textShadowNone],
   [colorSecondary, textShadowNone], // first shadow in line 3
@@ -44,31 +48,31 @@ const styleSequence = [
   [colorSecondary, textShadowNone],
   [colorPrimary, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_p1, textShadowNone],
   [colorSecondary, textShadowNone], // first shadow in line 4
-  [colorPrimary, textShadowNone],
+  [colorPrimary_p1, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_p1, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_p1, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_p1, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_p1, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_p2, textShadowNone],
   [colorSecondary, textShadowNone], // first shadow in line 5
-  [colorPrimary, textShadowNone],
+  [colorPrimary_p2, textShadowNone],
   [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
-  [colorSecondary, textShadowNone],
-  [colorPrimary, textShadow],
-  [colorPrimary, textShadowNone],
-  [colorSecondary, textShadowNone],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_p2, textShadowNone],
   [colorSecondary, textShadowNone],
   [colorPrimary, textShadow],
-  [colorPrimary, textShadowNone],
+  [colorPrimary_p2, textShadowNone],
+  [colorSecondary, textShadowNone],
+  [colorPrimary_p2, textShadowNone],
+  [colorSecondary, textShadowNone],
+  [colorPrimary, textShadow],
+  [colorPrimary_p2, textShadowNone],
   [colorSecondary, textShadowNone],
   [colorPrimary, textShadow],
   [colorSecondary, textShadowNone], // end of line 5 to the end
